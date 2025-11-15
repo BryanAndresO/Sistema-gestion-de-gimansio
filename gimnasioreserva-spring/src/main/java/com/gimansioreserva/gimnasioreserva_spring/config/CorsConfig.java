e6fb171bd3c0cc7146980b9d7c97b083ba32e854
Michael Villacrés <michael2003kiki@gmail.com>
2025-11-15 15:45:41 -0500

    Configuraci´on Spring Security

---
+++ gimnasioreserva-spring/src/main/java/com/gimansioreserva/gimnasioreserva_spring/config/CorsConfig.java
@@ -0,0 +1,27 @@
+package com.gimansioreserva.gimnasioreserva_spring.config.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+import java.util.List;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public CorsFilter corsFilter() {
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        // Cambia esto al puerto donde corre tu frontend React
+        config.setAllowedOrigins(List.of("http://localhost:5173", "http://localhost:3000"));
+        config.setAllowedHeaders(List.of("*"));
+        config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
+    }
+}
