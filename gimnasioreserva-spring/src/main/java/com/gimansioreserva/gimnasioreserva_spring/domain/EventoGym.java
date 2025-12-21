d5a3d19fc23be42cc6e8988386334239fca7f7e9
Michael Villacrés <michael2003kiki@gmail.com>
2025-12-21 13:14:07 -0500

    Domain EventoGym

--- gimnasioreserva-spring/src/main/java/com/gimansioreserva/gimnasioreserva_spring/domain/EventoGym.java
+++ gimnasioreserva-spring/src/main/java/com/gimansioreserva/gimnasioreserva_spring/domain/EventoGym.java
@@ -1,4 +1,27 @@
 package com.gimansioreserva.gimnasioreserva_spring.domain;
 
+import java.time.LocalDateTime;
+
 public class EventoGym {
+    private final String claseId;
+    private final TipoEvento tipo;
+    private final LocalDateTime timestamp;
+
+    public EventoGym(String claseId, TipoEvento tipo) {
+        this.claseId = claseId;
+        this.tipo = tipo;
+        this.timestamp = LocalDateTime.now();
+    }
+
+    public String getClaseId() {
+        return claseId;
+    }
+
+    public TipoEvento getTipo() {
+        return tipo;
+    }
+
+    public LocalDateTime getTimestamp() {
+        return timestamp;
+    }
 }
