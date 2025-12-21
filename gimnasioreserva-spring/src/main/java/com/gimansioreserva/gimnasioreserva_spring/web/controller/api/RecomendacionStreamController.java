0e95ac7ec588bd0480bd1703dc090fa121928459
Michael Villacrés <michael2003kiki@gmail.com>
2025-12-21 13:24:32 -0500

    RecomendacionStreamController

--- gimnasioreserva-spring/src/main/java/com/gimansioreserva/gimnasioreserva_spring/web/controller/api/RecomendacionStreamController.java
+++ gimnasioreserva-spring/src/main/java/com/gimansioreserva/gimnasioreserva_spring/web/controller/api/RecomendacionStreamController.java
@@ -1,4 +1,29 @@
 package com.gimansioreserva.gimnasioreserva_spring.web.controller.api;
 
+import com.gimansioreserva.gimnasioreserva_spring.domain.EventoGym;
+import com.gimansioreserva.gimnasioreserva_spring.service.core.EventoGymService;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+import reactor.core.publisher.Flux;
+
+/**
+ * Controller que actúa como Subscriber mediante SSE
+ */
+@RestController
+@RequestMapping("/api/recomendaciones")
 public class RecomendacionStreamController {
+
+    private final EventoGymService eventoGymService;
+
+    public RecomendacionStreamController(EventoGymService eventoGymService) {
+        this.eventoGymService = eventoGymService;
+    }
+
+    /**
+     * Cada cliente que se conecta es un Subscriber
+     */
+    @GetMapping(value = "/stream", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    public Flux<EventoGym> streamEventos() {
+        return eventoGymService.flujoEventos();
+    }
 }
