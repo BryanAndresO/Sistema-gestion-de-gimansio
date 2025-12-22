0813de2704a338d85bce417ab92c87c7d99a06c1
KarenYanez <121051519+KarenYanez@users.noreply.github.com>
2025-12-21 19:04:46 -0500

    Backend: métodos en RecomendacionDTO y RecomendacionService

--- gimnasioreserva-spring/src/main/java/com/gimansioreserva/gimnasioreserva_spring/dto/core/RecomendacionDTO.java
+++ gimnasioreserva-spring/src/main/java/com/gimansioreserva/gimnasioreserva_spring/dto/core/RecomendacionDTO.java
@@ -1,4 +1,35 @@
 package com.gimansioreserva.gimnasioreserva_spring.dto.core;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import java.time.LocalDateTime;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class RecomendacionDTO {
+    private final String claseId;
+    private final String mensaje;
+    private final Integer prioridad;
+    private final LocalDateTime timestamp;
+
+    public RecomendacionDTO(String claseId, String mensaje, Integer prioridad, LocalDateTime timestamp) {
+        this.claseId = claseId;
+        this.mensaje = mensaje;
+        this.prioridad = prioridad;
+        this.timestamp = timestamp;
+    }
+
+    public String getClaseId() {
+        return claseId;
+    }
+
+    public String getMensaje() {
+        return mensaje;
+    }
+
+    public Integer getPrioridad() {
+        return prioridad;
+    }
+
+    public LocalDateTime getTimestamp() {
+        return timestamp;
+    }
 }
