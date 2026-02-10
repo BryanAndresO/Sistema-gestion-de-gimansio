3334f520226fe41424840eca04f7a1a5d2137147
Michael Villacrés <michael2003kiki@gmail.com>
2026-02-10 14:59:18 -0500

    BeforeEach

--- gimnasioreserva-spring/src/test/java/com/gimansioreserva/gimnasioreserva_spring/ReservaServiceTest.java
+++ gimnasioreserva-spring/src/test/java/com/gimansioreserva/gimnasioreserva_spring/ReservaServiceTest.java
@@ -1,4 +1,42 @@
 package com.gimansioreserva.gimnasioreserva_spring;
 
+import com.gimansioreserva.gimnasioreserva_spring.mapper.ReservaMapper;
+import com.gimansioreserva.gimnasioreserva_spring.repository.ClaseRepository;
+import com.gimansioreserva.gimnasioreserva_spring.repository.ReservaRepository;
+import com.gimansioreserva.gimnasioreserva_spring.repository.UsuarioRepository;
+import com.gimansioreserva.gimnasioreserva_spring.service.core.EventoGymService;
+import com.gimansioreserva.gimnasioreserva_spring.service.core.ReservaService;
+import com.gimansioreserva.gimnasioreserva_spring.validator.ReservaValidator;
+import org.junit.jupiter.api.BeforeEach;
+
+import static org.mockito.Mockito.mock;
+
 public class ReservaServiceTest {
+    private ReservaRepository reservaRepository;
+    private ClaseRepository claseRepository;
+    private UsuarioRepository usuarioRepository;
+    private ReservaMapper reservaMapper;
+    private ReservaValidator reservaValidator;
+    private EventoGymService eventoGymService;
+
+    private ReservaService reservaService;
+
+    @BeforeEach
+    void setup() {
+        reservaRepository = mock(ReservaRepository.class);
+        claseRepository = mock(ClaseRepository.class);
+        usuarioRepository = mock(UsuarioRepository.class);
+        reservaMapper = mock(ReservaMapper.class);
+        reservaValidator = mock(ReservaValidator.class);
+        eventoGymService = mock(EventoGymService.class);
+
+        reservaService = new ReservaService(
+                reservaRepository,
+                claseRepository,
+                usuarioRepository,
+                reservaMapper,
+                reservaValidator,
+                eventoGymService
+        );
+    }
 }
